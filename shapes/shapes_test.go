cfe3ba6cfaea5fe1eecba69bea0dddb8b6bb6122
eelsvek <kev.sunlee@gmail.com>
2021-04-08 10:09:41 -0700

    shapes tests

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -1,6 +1,8 @@
 package shapes
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	t.Run("Rectangle", func(t *testing.T) {
@@ -16,13 +18,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10, 10}
-		got := rectangle.Area()
-		want := 100.0
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
+		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{2, 10}, want: 10.0},
+	}
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
 }
