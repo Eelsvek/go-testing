dc100047b8ab21177721da68a015293e7d665fad
eelsvek <kev.sunlee@gmail.com>
2021-04-11 10:56:16 -0700

    wallet test Deposit and Withdraw

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -19,21 +19,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		name  string
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72.0},
-		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{2, 10}, want: 10.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{2, 10}, hasArea: 10.0},
 	}
 
 	for _, tt := range areaTests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := tt.shape.Area()
 
-			if got != tt.want {
-				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 			}
 		})
 	}
